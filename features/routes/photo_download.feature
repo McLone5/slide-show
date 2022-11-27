08aa28c3916884c27ab545a7b06f0adcb5919514
Marc Sallaberremborde <marc.salla@infostrates.fr>
2022-11-27 20:20:41 +0100

    Improved photo test for the next feature

--- features/routes/photo_download.feature
+++ features/routes/photo_download.feature
@@ -2,4 +2,4 @@ Feature: Controlled file access (pass-thru, not direct access)
 
     Scenario:
         When I go to page "/photo_download/52/photo/test.jpg"
-        Then I get a photo
+        Then I get a photo with dimensions 640x427
--- tests/Behat/PhotoContext.php
+++ tests/Behat/PhotoContext.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Tests\Behat;
 
+use RuntimeException;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertStringStartsWith;
 
 final class PhotoContext extends AbstractContext
@@ -15,5 +19,24 @@ final class PhotoContext extends AbstractContext
     {
         $contentType = (string)$this->webContext->getCurrentClient()->getResponse()->headers->get('Content-Type');
         assertStringStartsWith('image/', $contentType);
+    }
+
+    /**
+     * @Then /^I get a photo with dimensions (\d+)x(\d+)$/
+     */
+    public function iGetAPhotoWithDimensions640x(int $expectedWidth, int $expectedHeight): void
+    {
+        $this->iGetAPhoto();
+
+        $binaryFileResponse = $this->webContext->getCurrentClient()->getResponse();
+        if (!$binaryFileResponse instanceof BinaryFileResponse) {
+            throw new RuntimeException('Unexpected response type');
+        }
+
+        [$actualWidth, $actualHeight] = getimagesize($binaryFileResponse->getFile()->getPathname())
+            ?: throw new RuntimeException('Unable to get image size')
+        ;
+
+        assertEquals([$expectedWidth, $expectedHeight], [$actualWidth, $actualHeight]);
     }
 }
